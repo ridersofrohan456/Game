4133936448ad22e5e80d5f90d1879660889f4a54
Rohan Kaul <rohankaul@Rohans-Mac-mini.local>
2025-11-15 16:05:19 -0800

    Refactor to use player class

--- main/main.lua
+++ main/main.lua
@@ -1,61 +1,14 @@
 function love.load()
-	image = love.graphics.newImage("images/walk.png")
-	local imageWidth = image:getWidth()
-	local imageHeight = image:getHeight()
+	Object = require("main.classic")
+	require("main.player")
 
-	numTotalFramesPerCycle = 8
-
-	frameWidth = imageWidth / numTotalFramesPerCycle
-	frameHeight = imageHeight / 3
-	walkingUpFrames = {}
-	walkingLeftFrames = {}
-	walkingRightFrames = {}
-	walkingDownFrames = {}
-
-	for i = 0, numTotalFramesPerCycle do
-		table.insert(
-			walkingRightFrames,
-			love.graphics.newQuad(i * frameWidth, 0, frameWidth, frameHeight, imageWidth, imageHeight)
-		)
-	end
-	for i = 0, numTotalFramesPerCycle do
-		table.insert(
-			walkingDownFrames,
-			love.graphics.newQuad(i * frameWidth, frameHeight, frameWidth, frameHeight, imageWidth, imageHeight)
-		)
-	end
-	for i = 0, numTotalFramesPerCycle do
-		table.insert(
-			walkingUpFrames,
-			love.graphics.newQuad(i * frameWidth, frameHeight * 2, frameWidth, frameHeight, imageWidth, imageHeight)
-		)
-	end
-	currFrame = 1
+	player = Player()
 end
 
 function love.update(dt)
-	animate(dt)
+	player:update(dt)
 end
 
 function love.draw()
-	frame = math.floor(currFrame)
-	if love.keyboard.isDown("right") then
-		love.graphics.draw(image, walkingRightFrames[frame], 100, 100)
-	elseif love.keyboard.isDown("left") then
-		love.graphics.draw(image, walkingRightFrames[frame], 100, 100, 0, -1, 1, frameWidth, 0)
-	elseif love.keyboard.isDown("up") then
-		love.graphics.draw(image, walkingUpFrames[frame], 100, 100)
-	elseif love.keyboard.isDown("down") then
-		love.graphics.draw(image, walkingDownFrames[frame], 100, 100)
-	else
-		love.graphics.draw(image, walkingDownFrames[4], 100, 100)
-		currFrame = 1
-	end
-end
-
-function animate(dt)
-	if currFrame > numTotalFramesPerCycle then
-		currFrame = 1
-	end
-	currFrame = currFrame + 10 * dt
+	player:draw()
 end
---
+++ main/player.lua
@@ -0,0 +1,106 @@
+-- Player Class --
+
+Player = Object:extend()
+
+function Player:new()
+	-- x, y are starting position coordinates --
+	self.x = love.graphics.getWidth() / 2
+	self.y = love.graphics.getHeight() / 2
+	self.speed = 100
+
+	-- animation frame data --
+	image = love.graphics.newImage("images/walk.png")
+	numFrames = 8
+	frameHeight = image:getHeight() / 3
+	frameWidth = image:getWidth() / numFrames
+
+	self.frames = {}
+	self.frames["right"] = {}
+	self.frames["up"] = {}
+	self.frames["down"] = {}
+	loadQuads(self)
+
+	currFrame = 1
+end
+
+function Player:update(dt)
+	-- animation updates --
+	if currFrame > numFrames or not isAnyDirectionalDown() then
+		currFrame = 1
+	else
+		currFrame = currFrame + 10 * dt
+	end
+
+	-- position updates --
+	if love.keyboard.isDown("up") then
+		self.y = self.y - self.speed * dt
+	elseif love.keyboard.isDown("down") then
+		self.y = self.y + self.speed * dt
+	elseif love.keyboard.isDown("left") then
+		self.x = self.x - self.speed * dt
+	elseif love.keyboard.isDown("right") then
+		self.x = self.x + self.speed * dt
+	end
+end
+
+function Player:draw()
+	local frame = math.floor(currFrame)
+	local scaling = 2
+
+	if love.keyboard.isDown("up") then
+		love.graphics.draw(image, self.frames["up"][frame], self.x, self.y, 0, scaling, scaling)
+	elseif love.keyboard.isDown("down") then
+		love.graphics.draw(image, self.frames["down"][frame], self.x, self.y, 0, scaling, scaling)
+	elseif love.keyboard.isDown("right") then
+		love.graphics.draw(image, self.frames["right"][frame], self.x, self.y, 0, scaling, scaling)
+	elseif love.keyboard.isDown("left") then
+		love.graphics.draw(image, self.frames["right"][frame], self.x, self.y, 0, -scaling, scaling, frameWidth, 0) -- flip right image
+	else
+		love.graphics.draw(image, self.frames["down"][1], self.x, self.y, 0, scaling, scaling)
+		currFrame = 1
+	end
+end
+
+function loadQuads(self)
+	for i = 0, numFrames do
+		table.insert(
+			self.frames["right"],
+			love.graphics.newQuad(i * frameWidth, 0, frameWidth, frameHeight, image:getWidth(), image:getHeight())
+		)
+	end
+
+	for i = 0, numFrames do
+		table.insert(
+			self.frames["down"],
+			love.graphics.newQuad(
+				i * frameWidth,
+				frameHeight,
+				frameWidth,
+				frameHeight,
+				image:getWidth(),
+				image:getHeight()
+			)
+		)
+	end
+
+	for i = 0, numFrames do
+		table.insert(
+			self.frames["up"],
+			love.graphics.newQuad(
+				i * frameWidth,
+				frameHeight * 2,
+				frameWidth,
+				frameHeight,
+				image:getWidth(),
+				image:getHeight()
+			)
+		)
+	end
+end
+
+function isAnyDirectionalDown()
+	return love.keyboard.isDown("up")
+		or love.keyboard.isDown("down")
+		or love.keyboard.isDown("left")
+		or love.keyboard.isDown("right")
+end
